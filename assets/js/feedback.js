f342ae09f65390fbd8ada15a221639c13be4558d
Matteo Falasca <matteo.falasca@emmek.org>
2023-07-18 17:19:39 +0200

    cleanup feedback.js

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -1,43 +1,40 @@
 const stars = document.querySelectorAll("#img-stelle > img");
 let clicked = false;
 
+remStarNext = element => {
+  while ((element = element.nextElementSibling)) {
+    element.classList.remove("selected-stars");
+  }
+};
+
+setStarPrev = element => {
+  while ((element = element.previousElementSibling)) {
+    element.classList.add("selected-stars");
+  }
+};
+
 const unColorStar = () => {
   if (clicked) {
     let element = document.querySelector(".clicked-star");
-    while ((element = element.nextElementSibling)) {
-      element.classList.remove("selected-stars");
-    }
-    element = document.querySelector(".clicked-star");
-    while ((element = element.previousElementSibling)) {
-      element.classList.add("selected-stars");
-    }
+    setStarPrev(element);
+    remStarNext(element);
   } else {
     stars.forEach(elm => elm.classList.remove("selected-stars"));
   }
 };
 
 const colorStar = event => {
-  unColorStar();
   let element = event.target;
   element.classList.add("selected-stars");
-  while ((element = element.previousElementSibling)) {
-    element.classList.add("selected-stars");
-  }
-  element = event.target;
-  while ((element = element.nextElementSibling)) {
-    element.classList.remove("selected-stars");
-  }
+  setStarPrev(element);
+  remStarNext(element);
 };
 
 const setStar = event => {
   let element = event.target;
   stars.forEach(elm => elm.classList.remove("clicked-star"));
-  stars.forEach(elm => elm.classList.remove("selected-stars"));
   element.classList.add("clicked-star");
   clicked = true;
-  while ((element = element.previousElementSibling)) {
-    element.classList.add("selected-stars");
-  }
 };
 
 stars.forEach(elm => (elm.onmouseover = colorStar));
