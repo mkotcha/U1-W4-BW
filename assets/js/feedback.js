f1231e974d4072fd96a3d8c459b806de595d60e2
Matteo Falasca <matteo.falasca@emmek.org>
2023-07-18 17:09:27 +0200

    stars work correctly

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -2,7 +2,16 @@ const stars = document.querySelectorAll("#img-stelle > img");
 let clicked = false;
 
 const unColorStar = () => {
-  if (!clicked) {
+  if (clicked) {
+    let element = document.querySelector(".clicked-star");
+    while ((element = element.nextElementSibling)) {
+      element.classList.remove("selected-stars");
+    }
+    element = document.querySelector(".clicked-star");
+    while ((element = element.previousElementSibling)) {
+      element.classList.add("selected-stars");
+    }
+  } else {
     stars.forEach(elm => elm.classList.remove("selected-stars"));
   }
 };
@@ -10,10 +19,11 @@ const unColorStar = () => {
 const colorStar = event => {
   unColorStar();
   let element = event.target;
-  event.target.classList.add("selected-stars");
+  element.classList.add("selected-stars");
   while ((element = element.previousElementSibling)) {
     element.classList.add("selected-stars");
   }
+  element = event.target;
   while ((element = element.nextElementSibling)) {
     element.classList.remove("selected-stars");
   }
@@ -22,11 +32,11 @@ const colorStar = event => {
 const setStar = event => {
   let element = event.target;
   stars.forEach(elm => elm.classList.remove("clicked-star"));
+  stars.forEach(elm => elm.classList.remove("selected-stars"));
   element.classList.add("clicked-star");
   clicked = true;
-
-  while ((element = element.nextElementSibling)) {
-    element.classList.remove("selected-stars");
+  while ((element = element.previousElementSibling)) {
+    element.classList.add("selected-stars");
   }
 };
 
